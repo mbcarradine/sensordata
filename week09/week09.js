af9b40722768e9579c7afbfc518e7f4cd1faccf8
MBCarradine <carradim@newschool.edu>
2017-10-29 17:18:43 -0400

    20171029

--- week09/week09.js
+++ week09/week09.js
@@ -2,25 +2,21 @@ var request = require('request');
 const { Client } = require('pg');
 
 // PARTICLE PHOTON
-var device_id = '260038000651353530373132';
-var access_token = 'a525c2297e901f27e9e6b8737f77cbc26f56371e';
-//var device_id = process.env.PHOTON_ID;
-//var access_token = process.env.PHOTON_TOKEN;
+
+var device_id = process.env.PHOTON_ID;
+var access_token = process.env.PHOTON_TOKEN;
 var particle_variable = 'json';
-//var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
-var device_url= 'https://api.particle.io/v1/devices/260038000651353530373132/json?access_token=a525c2297e901f27e9e6b8737f77cbc26f56371e';
+var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
 //console.log(device_url);
-// AWS RDS POSTGRESQL INSTANCE
 
 
 // Make environment variables
 
 var db_credentials = new Object();
 db_credentials.user = 'mbcarradine';
-//db_credentials.host = process.env.AWSRDS_EP;
-db_credentials.host = 'datastructures.cwa4xwjfcya6.us-east-1.rds.amazonaws.com';
+db_credentials.host = process.env.AWSRDS_EP; //the endpoint
 db_credentials.database = 'fernsaysdb';
-db_credentials.password = process.env.AWSRDS_P;
+db_credentials.password = process.env.AWSRDS_P; //the max password
 db_credentials.port = 5432;
 
 var getAndWriteData = function() {
@@ -30,8 +26,6 @@ var getAndWriteData = function() {
         var device_json_string = JSON.parse(body).result;
         var tiltsensor = JSON.parse(device_json_string).tiltvalue;
         var tempsensor = JSON.parse(device_json_string).TempCvalue;
-      //  console.log(tempsensor);
-
 
         // Connect to the AWS RDS Postgres database
         const client = new Client(db_credentials);
